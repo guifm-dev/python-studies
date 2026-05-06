e30d763941332ae50391df79b5395cb20c4f96b7
Guilherme Fortunato <guilhermefortunato671@gmail.com>
2026-05-05 22:06:24 -0300

    dunder objects & dunder main

---
+++ modules/dunder-objects.py
@@ -0,0 +1,18 @@
+"""
+Objetos Dunder
+Dunder - Duplo Underscore - Duplo Underline
+Atributos Mágicos - variáveis
+Metódos Mágicos - funções
+
+__init__ - Python 2.x
+__file__
+
+__name__
+__main__
+
+__doc__
+
+"""
+
+if __name__ == "__main__":
+    print("modulo rodando no main")
